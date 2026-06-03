e5f512b0be2808934260b85f8eed8e7a60742ef7
Mirjam <mirjam.mclachlan@gmail.com>
2026-06-03 22:45:21 +0200

    feat(cats): add new spritesheets to CatList

--- app/src/main/java/com/example/purrsistence/domain/cats/CatList.kt
+++ app/src/main/java/com/example/purrsistence/domain/cats/CatList.kt
@@ -7,16 +7,26 @@ import com.example.purrsistence.ui.components.animation.SpriteSheetData
 object CatList {
     // List of all cats the user can buy in the Shop
     val cats = listOf(
-        ShopItem("cat_1", "House Cat", 0, R.drawable.cat_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
-        ShopItem("cat_2", "Black Cat", 13, R.drawable.cat_black),
-        ShopItem("cat_3", "Creme Cat", 20, R.drawable.cat_creme),
-        ShopItem("cat_4", "Brown Cat", 10, R.drawable.cat_brown),
-        ShopItem("cat_5", "Gray Cat", 15, R.drawable.cat_darkgray),
-        ShopItem("cat_6", "White Cat", 2, R.drawable.cat_lightgray),
-        ShopItem("cat_7", "Orange Cat", 3, R.drawable.cat_orange),
-        ShopItem("cat_8", "Spotted Cat", 2, R.drawable.cat_spotted),
-        ShopItem("cat_9", "Siamese Cat", 50, R.drawable.cat_siamese),
-        ShopItem("cat_10", "Lucky Cat", 777, R.drawable.cat_lucky)
+        ShopItem("cat_1", "House Cat", 0,
+            R.drawable.default_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_2", "Black Cat", 13,
+            R.drawable.black_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_3", "Creme Cat", 20,
+            R.drawable.creme_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_4", "Brown Cat", 10,
+            R.drawable.brown_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_5", "Gray Cat", 15,
+            R.drawable.gray_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_6", "White Cat", 2,
+            R.drawable.white_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_7", "Orange Cat", 3,
+            R.drawable.orange_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_8", "Spotted Cat", 2,
+            R.drawable.spotted_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_9", "Siamese Cat", 50,
+            R.drawable.siamese_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L)),
+        ShopItem("cat_10", "Lucky Cat", 777,
+            R.drawable.lucky_idle,SpriteSheetData(columns = 4, rows = 3, totalFrames = 10, frameDurationMs = 400L))
     )
 
     private val catMap = cats.associateBy { it.id }
