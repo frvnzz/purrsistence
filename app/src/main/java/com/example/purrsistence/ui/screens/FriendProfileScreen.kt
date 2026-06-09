4ae8d591e64ae8a49f8f9027878eff45c9a5a621
cc231016 <cc231016@ustp-students.at>
2026-06-09 14:48:37 +0200

    fix(dev): Cat image bug

--- app/src/main/java/com/example/purrsistence/ui/screens/FriendProfileScreen.kt
+++ app/src/main/java/com/example/purrsistence/ui/screens/FriendProfileScreen.kt
@@ -1,18 +1,33 @@
 package com.example.purrsistence.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.purrsistence.domain.cats.CatList
 import com.example.purrsistence.domain.model.FriendProfileDetails
 import com.example.purrsistence.domain.model.ShopItem
+import com.example.purrsistence.ui.components.homeScreen.CatImage
 import com.example.purrsistence.ui.state.TopBarState
 import com.example.purrsistence.ui.theme.Spacing
 import com.example.purrsistence.ui.viewmodel.FriendViewModel
@@ -159,9 +174,9 @@ fun FriendCatItem(
                 .padding(Spacing.md),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = cat.imageRes),
-                contentDescription = cat.name,
+            CatImage(
+                cat = cat,
+                isAnimated = false,
                 modifier = Modifier.size(64.dp)
             )
 
